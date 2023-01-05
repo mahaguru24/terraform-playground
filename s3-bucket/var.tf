17afb9af1d94ee7f8de388fbf1eb804275f8f53a
Guruprasad Mulay <guruprasad.mulay@banxa.com>
2023-01-05 15:57:48 +1100

    Create s3-bucket

---
+++ s3-bucket/var.tf
@@ -0,0 +1,18 @@
+variable "bucket_name" {
+    type = string
+}
+
+variable "acl_value" {
+    type = string
+}
+
+variable "aws_access_key" {
+    type = string
+}
+variable "aws_secret_key" {
+    type = string
+}
+
+variable "region" {
+    type = string
+}
